c9d3bb5cbf9845e6eb6f8e5edcc3e2d3ce978ecc
david-6399 <chdaoud1700@gmail.com>
2025-11-29 20:10:46 +0100

    install breeze

--- routes/api.php
+++ routes/api.php
@@ -6,3 +6,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+route::prefix('v1')->group(function () {
+    route::get('room', function(){
+        return 'room one';
+    }) ;
+});
+
+require __DIR__ . '/auth.php';
